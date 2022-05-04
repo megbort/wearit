2d791bf3f3bdf2185908c4b66931a57852bb6c65
Megan Krenbrink <megan.krenbrink@gmail.com>
2022-05-03 17:06:45 -0700

    Various Updates
    - Fixed navigation
    - More styles
    - Replacing reset.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,44 @@
 // NAVIGATION MANU
 
+// Toggle nav menu
 function toggleMenu() {
-    var menu = document.getElementById("myLinks");
-        if (menu.style.display === "block") {
-            menu.style.display = "none";
-        } else {
-            menu.style.display = "block";
-    }
+  var menu = document.getElementById("myLinks");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 }
 
+// change from desktop to mobile menu layout on window size event
 function updateNav(windowSize) {
-    if (windowSize.matches) { // If media query matches
-        document.getElementById("myLinks").style.display = "none";
-    } else {
-        document.getElementById("myLinks").style.display = "block";
-        document.getElementById("myLinks").style.textAlign = "right";
-    }
+  if (windowSize.matches) {
+    document.getElementById("myLinks").style.display = "none";
+    console.log('somthing');
+  } else {
+    document.getElementById("myLinks").style.display = "block";
+    document.getElementById("myLinks").style.textAlign = "right";
+  }
 }
 
-// Call listener function at run time
-var windowSize = window.matchMedia("(max-width: 670px)"); 
-// Attach listener function when the window size changes    
-windowSize.addListener(updateNav); 
+// Store value with the window size and run funtion at 670px 
+let windowSize = window.matchMedia("(max-width: 670px)");
+windowSize.addEventListener('change', updateNav);
 
+// Keep the mobile nav closed on page load
+function closeNav() {
+  if (windowSize.matches) {
+  document.getElementById("myLinks").style.display = "none";
+  console.log(windowSize);
+  }
+}
 
 // FLICKITY SLIDER
-
 $('.main-carousel').flickity({
-  // options
   cellAlign: 'center',
   contain: true,
   wrapAround: true,
   autoPlay: true,
-  pauseAutoPlayOnHover: false 
+  pauseAutoPlayOnHover: false,
+  pageDots: false
 });
